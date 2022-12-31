1a6140741ffcdedebe56a3af286108f8910d798f
SeeLog <seelog693@gmail.com>
2023-01-01 03:40:20 +0900

    [add] EBNF tree

--- slcc.c
+++ slcc.c
@@ -5,6 +5,87 @@
 #include <stdlib.h>
 #include <string.h>
 
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // 0-9
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;  // ND_NUM のときに使われる
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+// expr = mul ("+" mul | "-"  mul)*
+Node *expr() {
+  Node *node = mul();
+
+  for(;;) {
+    if (consume('+')) {
+      node = new_node(ND_ADD, node, mul());
+    }
+    else if (consume('-')) {
+      node = new_node(ND_SUB, node, mul());
+    }
+    else {
+      return node;
+    }
+  }
+}
+
+// mul = primary ( "*" primary | "/" primary )*
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+    if (consume('*')) {
+      node = new_node(ND_MUL, node, primary());
+    }
+    else if (consume('/')) {
+      node = new_node(ND_DIV, node, primary());
+    }
+    else {
+      return node;
+    }
+  }
+}
+
+// primary = num | "(" expr ")"
+Node *primary() {
+  // ( が来たら "(" expr ")" が来るはず、来なければ受理できない
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  // そうでなければ数値を受理
+  return new_node_num(expect_number());
+}
+
 // トークンの種類
 typedef enum {
   TK_RESERVED, // 記号
